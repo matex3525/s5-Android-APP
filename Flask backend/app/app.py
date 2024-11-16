c3f0390512f6f5a3c7a2e81507d835f137dce059
TheHyper45 <dawid.priv@o2.pl>
2024-11-16 13:55:27 +0100

    Added transaction to backend.

--- Flask backend/app/app.py
+++ Flask backend/app/app.py
@@ -3,7 +3,7 @@ import redis
 import secrets
 
 app = Flask(__name__)
-redis_client = redis.StrictRedis(host='redis', port=6379, decode_responses=True)
+redis_client = redis.StrictRedis(host = "redis",port = 6379,decode_responses = True)
 
 @app.get("/list")
 def endpoint_list_events():
@@ -16,21 +16,18 @@ def endpoint_list_events():
 def endpoint_event():
     new_key = "event_" + str(redis_client.incr("event_incr"))
     admin_token = secrets.token_hex(3)
-    
-    #@TODO: This should probably use a transaction.
-    is_created = redis_client.sadd("event_names", new_key)
-    if not is_created:
+    try:
+        transaction = redis_client.pipeline()
+        transaction.sadd("event_names",new_key)
+        transaction.hset("tokens",new_key,admin_token)
+        transaction.execute(raise_on_error = True)
+        return {"event": new_key,"admin_token": admin_token},201
+    except Exception as error:
+        print(str(error))
         return {"error": "Internal error."},400
-
-    is_created = redis_client.hset("tokens",new_key,admin_token)
-    if not is_created:
-        redis_client.srem("event_names",new_key)
-        return {"error": "Internal error."},400
-
-    return {"event": new_key,"admin_token": admin_token},201
 
 def does_event_exist(event_name):
-    return redis_client.sismember("event_names", event_name)
+    return redis_client.sismember("event_names",event_name)
 
 def is_admin_token_valid(event_name,token):
     return redis_client.hget("tokens",event_name) == token
@@ -39,23 +36,26 @@ def is_admin_token_valid(event_name,token):
 def endpoint_event_delete(event_name):
     if not does_event_exist(event_name):
         return {"error": f"There's no event '{event_name}'."},400
-
     if "token" not in request.json:
         return {"error": "'token' not in request."},400
-
     if not is_admin_token_valid(event_name,request.json["token"]):
         return {"error": "Invalid token."},400
 
-    redis_client.xtrim(event_name,minid='0-0')
-    redis_client.hdel("tokens",event_name)
-    redis_client.srem("event_names",event_name)
-    return {"success": 1},200
+    try:
+        transaction = redis_client.pipeline()
+        transaction.xtrim(event_name,minid = "0-0")
+        transaction.hdel("tokens",event_name)
+        transaction.srem("event_names",event_name)
+        transaction.execute(raise_on_error = True)
+        return {"success": 1},200
+    except Exception as error:
+        print(str(error))
+        return {"error": "Internal error."},400
 
 @app.post("/images/<event_name>")
 def endpoint_images_add(event_name):
     if not does_event_exist(event_name):
         return {"error": f"There's no event '{event_name}'."},400
-    
     if "b64" not in request.json:
         return {"error": "'b64' not in request."},400
 
@@ -73,17 +73,14 @@ def endpoint_images_get(event_name,image_id):
         temp = {"image_id": image_id}
         temp.update(attributes)
         data["images"].append(temp)
-
     return data,200
 
 @app.delete("/images/<event_name>/<image_id>")
 def endpoint_images_delete(event_name,image_id):
     if not does_event_exist(event_name):
         return {"error": f"There's no event '{event_name}'."},400
-
     if "token" not in request.json:
         return {"error": "'token' not in request."},400
-
     if not is_admin_token_valid(event_name,request.json["token"]):
         return {"error": "Invalid token."},400
 
