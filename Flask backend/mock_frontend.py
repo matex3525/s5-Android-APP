62ea6af264d04888178f94564a85b0723d1c6ce4
TheHyper45 <dawid.priv@o2.pl>
2024-11-23 23:02:00 +0100

    Added ability to add titles (strings) to images.

--- Flask backend/mock_frontend.py
+++ Flask backend/mock_frontend.py
@@ -3,7 +3,7 @@ import base64
 import requests
 
 address = "http://127.0.0.1:8080"
-def make_http_request(method,string,json):
+def make_http_request(method,string,json,printResponse = True):
     print(f"Request: {string}")
     url = f"{address}/{string}"
     response = None
@@ -17,7 +17,8 @@ def make_http_request(method,string,json):
         response = requests.delete(url = url,json = json)
     assert response != None
     data = response.json()
-    print(f"Response: {data}")
+    if printResponse:
+        print(f"Response: {data}\n")
     return data
 
 class Commands:
@@ -39,45 +40,51 @@ class Commands:
 
     @staticmethod
     def genevent(name,*args):
-        if len(args) != 0:
-            raise Exception("Syntax: genevent")
-        make_http_request("post","event",{})
+        if len(args) != 1:
+            raise Exception("Syntax: genevent <event_name>")
+        make_http_request("post","event",{"event_name": args[0]})
 
     @staticmethod
     def delevent(name,*args):
         if len(args) != 2:
-            raise Exception("Syntax: delevent <event_name> <admin_token>")
-        make_http_request("delete",f"event/{args[0]}",{"token": args[1]})
+            raise Exception("Syntax: delevent <user_token> <admin_token>")
+        make_http_request("delete",f"event/{args[0]}",{"admin_token": args[1]})
 
     @staticmethod
     def listevents(name,*args):
         if len(args) != 0:
             raise Exception("Syntax: listevents")
-        make_http_request("get","list",{})
+        make_http_request("get","list",{"secret_key": "Konstytucja Rzeczypospolitej Polskiej"})
 
     @staticmethod
     def addimage(name,*args):
-        if len(args) != 2:
-            raise Exception("Syntax: addimage <event_name> <path_to_image>")
+        if len(args) != 2 and len(args) != 3:
+            raise Exception("Syntax: addimage <user_token> <path_to_image> [title]")
         with open(args[1],"rb") as file:
             content = base64.b64encode(file.read()).decode("utf-8")
-            make_http_request("post",f"images/{args[0]}",{"b64": content})
+            if len(args) == 2:
+                make_http_request("post",f"images/{args[0]}",{"b64": content})
+            else:
+                make_http_request("post",f"images/{args[0]}",{"b64": content,"title": args[2]})
 
     @staticmethod
     def remimage(name,*args):
         if len(args) != 3:
-            raise Exception("Syntax: remimage <event_name> <image_id> <admin_token>")
-        make_http_request("delete",f"images/{args[0]}/{args[1]}",{"token": args[2]})
+            raise Exception("Syntax: remimage <user_token> <image_id> <admin_token>")
+        make_http_request("delete",f"images/{args[0]}/{args[1]}",{"admin_token": args[2]})
 
     @staticmethod
     def getimage(name,*args):
-        if len(args) < 2 or len(args) > 3:
-            raise Exception("Syntax: getimage <event_name> <image_id> [path_to_output_image]")
-        data = make_http_request("get",f"images/{args[0]}/{args[1]}",{})["data"]["images"]
-        if len(data) == 1 and len(args) == 3:
-            content = base64.b64decode(bytes(data[0]["b64"],encoding = "utf-8"))
-            with open(args[2],"wb") as file:
-                file.write(content)
+        if len(args) != 2 and len(args) != 3:
+            raise Exception("Syntax: getimage <user_token> <image_id> [images_to_file]")
+        images = make_http_request("get",f"images/{args[0]}/{args[1]}",{},False)["images"]
+        for image in images:
+            print(f"{{\"image_id\": \"{image["image_id"]}\",\"title\": \"{image["title"]}\",\"b64\": [len: {len(image["b64"])}]}}")
+            if len(args) == 3:
+                content = base64.b64decode(bytes(image["b64"],encoding = "utf-8"))
+                with open(f"{image["image_id"]}.jpg","wb") as file:
+                    file.write(content)
+        print()
 
 if __name__ == "__main__":
     allCommands = [getattr(Commands,x) for x in dir(Commands) if x[0] != '_']
