f956719f6ef72e6a8f42757995f8cc5e31a51ce1
PawelCZ1 <pawelczapran@gmail.com>
2025-01-12 19:57:05 +0100

    #7 Snackbar implemented

--- s5app/app/src/main/java/com/example/s5app/MainActivity.kt
+++ s5app/app/src/main/java/com/example/s5app/MainActivity.kt
@@ -5,12 +5,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
@@ -69,16 +72,28 @@ fun MainActivityScreen(startDestination: Any) {
                 )
             )
         },
-        snackbarHost = { SnackbarHost(hostState = snackbarHostState) } // Użycie SnackbarHostState
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState) { data ->
+                Snackbar(
+                    snackbarData = data,
+                    containerColor = MaterialTheme.colorScheme.primary, // Ustawiamy niestandardowy kolor tła
+                    contentColor = MaterialTheme.colorScheme.onPrimary  // Kontrastowy kolor tekstu
+                )
+            }
+        }
     ) {
         // Obsługa braku połączenia z internetem
         LaunchedEffect(isInternetAvailable) {
             if (!isInternetAvailable) {
                 scope.launch {
                     snackbarHostState.showSnackbar(
-                        message = "Nie masz połączenia z Internetem.",
-                        actionLabel = "OK"
+                        message = "No internet connection",
+                        duration = SnackbarDuration.Indefinite
                     )
+                }
+            } else {
+                scope.launch {
+                    snackbarHostState.currentSnackbarData?.dismiss() // Ukryj aktualnego Snackbara
                 }
             }
         }
